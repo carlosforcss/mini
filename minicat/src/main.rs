e6c94d660f7f5972390f60c3b04a5be393814624
= <=>
2026-02-02 15:41:28 -0600

    feat: [minicat] stderr impleemntation

--- minicat/src/main.rs
+++ minicat/src/main.rs
@@ -1,12 +1,19 @@
 use std::fs;
 use std::env;
 
-fn get_file_content(file_name: &str) -> Result<String, std::io::Error> {
-    fs::read_to_string(file_name)
-}
-
 fn main() {
-    let filename = env::args().nth(1).expect("Please provilde a file name as an argument");
-    let content = get_file_content(&filename).expect("Could not read the file"); 
-    print!("{}", content);
+    let filename = match env::args().nth(1) {
+        Some(name) => name,
+        None => {
+            eprintln!("Please provide a filename as an argument");
+            return;
+        }
+    };
+    match fs::read_to_string(&filename) {
+        Ok(content) => print!("{}", content),
+        Err(_) => {
+            eprintln!("Error: cat: {}: No such file or direectory", filename);
+            return;
+        }
+    }; 
 }
